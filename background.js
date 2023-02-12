c392dcec5121d85a611bc04b3f0671435d7e1893
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-02-12 21:53:24 +0100

    Add Chrome compatibility

--- background.js
+++ background.js
@@ -1,13 +1,15 @@
-browser.menus.create({
+const browserApi = typeof browser !== 'undefined' ? browser : chrome;
+
+browserApi.contextMenus.create({
   id: 'ichi_moe_lookup',
   title: 'Look up on ichi.moe',
   contexts: ['selection']
 });
 
-browser.menus.onClicked.addListener((info) => {
+browserApi.contextMenus.onClicked.addListener((info) => {
   if (info.menuItemId === 'ichi_moe_lookup') {
     const selection = info.selectionText;
-    browser.tabs.create({
+    browserApi.tabs.create({
       url: `https://ichi.moe/cl/qr/?q=+${selection}+&r=htr`
     });
   }
